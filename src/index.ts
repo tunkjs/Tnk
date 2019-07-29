891e7ad5e643c8d8eeb85036647718af5ec61f0b
steven-JC <522702986@qq.com>
2019-07-30 00:05:31 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,41 @@
 export default class MediatorX {
     protected subscribers: { [key: string]: Subscriber[] } = {}
     protected store: Store
-    protected isolation: boolean
+    protected hooks: Hooks
+    protected isolate: boolean
 
-    constructor(store: Store, isolation: boolean = true) {
-        this.isolation = isolation
+    constructor(store: Store, hooks?: Hooks, isolate: boolean = true) {
+        this.isolate = isolate
+        this.hooks = {
+            willPublish: (channel: string, data: State, isolate?: boolean) =>
+                true,
+            willSetState: (
+                actionName: string,
+                fieldName: string,
+                newState: State,
+                isolate: boolean
+            ) => newState,
+            didSetState: (
+                actionName: string,
+                fieldName: string,
+                newState: State,
+                store: Store
+            ) => {},
+            ...hooks
+        }
         if (!store) throw DEFAULT_DATA_ERR_MSG
         this.store = this.clone(store) as Store
     }
-    public getState(fieldName: string) {
-        return this.clone(this.store[fieldName])
+
+    public getState(fieldName: string, isolate: boolean = true) {
+        return this.clone(this.store[fieldName], isolate)
     }
+
     public setState(
         actionName: string,
         fieldName: string,
-        newState: State,
-        isolation?: boolean
+        newState: State | StateHandler,
+        isolate: boolean = true
     ) {
         if (typeof this.store[fieldName] === 'undefined') {
             throw new Error(
@@ -24,18 +44,63 @@ export default class MediatorX {
                 )}`
             )
         }
-        this.store[fieldName] = this.clone(newState, isolation)
-        const subscribers = this.subscribers[actionName]
-        if (!subscribers || !subscribers.length) return
-        let i = subscribers.length
-        while (i--) subscribers[i](this.clone(newState, isolation))
+
+        const set = (newState) => {
+            const newStat: State | undefined =
+                this.hooks.willSetState &&
+                this.hooks.willSetState.call(
+                    this,
+                    actionName,
+                    fieldName,
+                    newState,
+                    isolate
+                )
+            if (typeof newStat === 'undefined') {
+                return
+            }
+            this.store[fieldName] = this.clone(newStat, isolate)
+            const subscribers = this.subscribers[actionName]
+            if (!subscribers || !subscribers.length) return
+            let i = subscribers.length
+            while (i--) subscribers[i](this.clone(newStat, isolate))
+            this.hooks.didSetState &&
+                this.hooks.didSetState.call(
+                    this,
+                    actionName,
+                    fieldName,
+                    newState,
+                    this.store
+                )
+        }
+
+        let res
+        if (typeof newState === 'function') {
+            res = newState(this.clone(this.store[fieldName], isolate))
+        } else {
+            res = newState
+        }
+        if (res && res.then) {
+            res.then((newState) => {
+                if (typeof newState === 'undefined') throw UNDEFINED_ERR_MSG
+                set(newState)
+            })
+        } else {
+            set(res)
+        }
     }
-    public pub(channel: string, data: PlainObject) {
+
+    public pub(channel: string, data: State, isolate: boolean = true) {
         const subscribers = this.subscribers[channel]
         if (!subscribers || !subscribers.length) return
-        let i = subscribers.length
-        while (i--) subscribers[i](this.clone(data))
+        if (
+            this.hooks.willPublish &&
+            !!this.hooks.willPublish.call(this, channel, data, isolate)
+        ) {
+            let i = subscribers.length
+            while (i--) subscribers[i](this.clone(data, isolate))
+        }
     }
+
     public sub(channel: string, subscriber: Subscriber) {
         const subscribers = (this.subscribers[channel] =
             this.subscribers[channel] || [])
@@ -49,9 +114,9 @@ export default class MediatorX {
 
     protected clone(
         state: State | Store,
-        isolation: boolean = true
+        isolate: boolean = true
     ): State | Store {
-        if (!this.isolation || !isolation) {
+        if (!this.isolate || !isolate) {
             return state
         }
         if (state && typeof state === 'object') {
@@ -62,6 +127,7 @@ export default class MediatorX {
 
 const DEFAULT_DATA_ERR_MSG =
     '[MediatorX] you should define the fields of store when instantiate MediatorX'
+const UNDEFINED_ERR_MSG = '[MediatorX] new state should not be undefined'
 declare type Store = { [fieldName: string]: State }
 declare type State =
     | number
@@ -75,6 +141,24 @@ declare type State =
     | null
     | null[]
 
+declare interface Hooks {
+    willPublish?(channel: string, data: State, isolate?: boolean): boolean
+    willSetState?(
+        actionName: string,
+        fieldName: string,
+        newState: State,
+        isolate?: boolean
+    ): State | undefined
+    didSetState?(
+        actionName: string,
+        fieldName: string,
+        newState: State,
+        store?: Store
+    ): void
+}
+declare interface StateHandler {
+    (data: State): State
+}
 declare interface Subscriber {
     (data: State): void
 }
